1d10e697e11dd4603093650c4f8f3f8af55f5e58
Dima Privalov <gontel.dim@gmail.com>
2024-09-29 22:41:32 +0500

    init structure 2

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+#include "../models/fileSchema.h"
+
+int main(int argc, char const *argv[])
+{
+    // setlocale(LC_ALL, "Russian");
+    cout << "Хахахах";
+    return 0;
+}
