1fa4d3c91ddf29b6a91a3ebe97648dbc9efca1b9
Dima Privalov <gontel.dim@gmail.com>
2024-10-09 21:14:29 +0500

    просто

---
+++ tests/unit_test_outputDB.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "../src/getFileFromDir/getFileFromDir.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
