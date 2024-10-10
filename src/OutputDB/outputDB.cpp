8ed6f93def0a6e01430d1b97bebaa237306e3e08
PapuaGvinea145 <p0chta1234prosto@yandex.ru>
2024-10-10 20:13:38 +0500

    constructor for output db

---
+++ src/OutputDB/outputDB.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "outputDB.h"
+
+using namespace std;
+
+int main()
+{
+    OutputDB OurDatabase;
+}
---
+++ src/OutputDB/outputDB.h
@@ -0,0 +1,33 @@
+#ifndef OUTPUT_DB_H
+#define OUTPUT_DB_H
+
+#include "../../lib/sqlite3/sqlite3.h"
+#include <iostream>
+
+using namespace std;
+
+class OutputDB
+{
+    private:
+        sqlite3 *DB;
+    public:
+        OutputDB()
+        {
+            sqlite3_open("output.db", &DB);
+            string create_known_table = "CREATE TABLE KNOWN_FILES ("
+                                "ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+                                "HASH TEXT,"
+                                "PATH TEXT"
+                                ");";
+            string create_unknown_table = "CREATE TABLE UNKNOWN_FILES ("
+                                "ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+                                "HASH TEXT,"
+                                "PATH TEXT"
+                                ");";            
+            char *errMsg;
+            sqlite3_exec(DB, create_known_table.c_str(), nullptr, nullptr, &errMsg);
+            sqlite3_exec(DB,create_unknown_table.c_str(),nullptr,nullptr,&errMsg);
+            cout << errMsg << "\n";
+        };
+};
+#endif //OUTPUT_DB_H
