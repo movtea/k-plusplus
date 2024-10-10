54cae902e67344583faa3a836927ff51d46e8f2f
PapuaGvinea145 <p0chta1234prosto@yandex.ru>
2024-10-10 17:01:54 +0500

    Change filestruct

--- src/OutputDB/output.cpp
+++ src/OutputDB/output.cpp
@@ -5,34 +5,27 @@
 #include "../nsrlRepository/nsrlRepository.h"
 #include "../getFileFromDir/getFileFromDir.h"
 
-void OutputDB:FillTheDB(vector<FilePtr> OurFiles)
+void OutputDB:FillTheDB(FilePtr File)
 {
     sqlite3_stmt* stmt;
-    //string hash = File->hash_md5;
-    //string path = File->path;
-    //vector<FilePtr> OurFiles = getFileFromDir()
-    int i = 0;
-    while (i < OurFiles.size())
+    if (File->IsINDB == true)
     {
-        if (BIGBALL.IsHashInDB(OurFiles[i]) == true)
-        {
-            string sql = "INSERT INTO KNOWN_FILES (hash,path)"
+        string sql = "INSERT INTO KNOWN_FILES (hash,path)"
                                 "VALUES (:hash, :path);";
-            sqlite3_prepare_v2(DB, sql.c_str(), -1, &stmt, NULL);
-            sqlite3_bind_text(stmt, sqlite3_bind_parameter_index(stmt, ":hash"), hash, -1, SQLITE_TRANSIENT);
-            sqlite3_bind_text(stmt, sqlite3_bind_parameter_index(stmt, ":path"), path, -1, SQLITE_TRANSIENT);
-            sqlite3_step(stmt);
-            sqlite3_finalize(stmt);
-        }
-        else
-        {
-            string sql = "INSERT INTO UNKNOWN_FILES (hash,path)"
+        sqlite3_prepare_v2(DB, sql.c_str(), -1, &stmt, NULL);
+        sqlite3_bind_text(stmt, sqlite3_bind_parameter_index(stmt, ":hash"), hash, -1, SQLITE_TRANSIENT);
+        sqlite3_bind_text(stmt, sqlite3_bind_parameter_index(stmt, ":path"), path, -1, SQLITE_TRANSIENT);
+        sqlite3_step(stmt);
+        sqlite3_finalize(stmt);
+    }
+    else
+    {
+        string sql = "INSERT INTO UNKNOWN_FILES (hash,path)"
                                 "VALUES (:hash, :path);";
-            sqlite3_prepare_v2(DB, sql.c_str(), -1, &stmt, NULL);
-            sqlite3_bind_text(stmt, sqlite3_bind_parameter_index(stmt, ":hash"), hash, -1, SQLITE_TRANSIENT);
-            sqlite3_bind_text(stmt, sqlite3_bind_parameter_index(stmt, ":path"), path, -1, SQLITE_TRANSIENT);
-            sqlite3_step(stmt);
-            sqlite3_finalize(stmt);
-        };
+        sqlite3_prepare_v2(DB, sql.c_str(), -1, &stmt, NULL);
+        sqlite3_bind_text(stmt, sqlite3_bind_parameter_index(stmt, ":hash"), hash, -1, SQLITE_TRANSIENT);
+        sqlite3_bind_text(stmt, sqlite3_bind_parameter_index(stmt, ":path"), path, -1, SQLITE_TRANSIENT);
+        sqlite3_step(stmt);
+        sqlite3_finalize(stmt);
     };
-}
+};
