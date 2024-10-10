8673ffd8ef714c58fcb25f048b2f2ff339923b36
PapuaGvinea145 <p0chta1234prosto@yandex.ru>
2024-10-10 21:08:14 +0500

    unknow file sql insert(still hate sqlite3 lib)

--- src/OutputDB/outputDB.cpp
+++ src/OutputDB/outputDB.cpp
@@ -12,16 +12,30 @@ void OutputDB::FillTheDB(FilePtr ourfile)
         sqlite3_prepare_v2(DB, sql.c_str(), -1, &stmt, NULL);
         sqlite3_step(stmt);
         sqlite3_finalize(stmt);
+    }
+    else
+    {
+        sqlite3_stmt* stmt;
+        string sql = "INSERT INTO UNKNOWN_FILES (HASH,PATH) VALUES ('"+ourfile->hash_sha1+"','"+ourfile->path+"');";
+        //cout << sql << "\n";
+        sqlite3_prepare_v2(DB, sql.c_str(), -1, &stmt, NULL);
+        sqlite3_step(stmt);
+        sqlite3_finalize(stmt);
     };
 };
 
 int main()
 {
     OutputDB OurDatabase;
-    FilePtr some_file = new File();
-    some_file->hash_sha1 = "TEST_HASH";
-    some_file->path = "TEST_PATH";
-    some_file->IsInDB = true;
-    cout << some_file->path << "\n";
-    OurDatabase.FillTheDB(some_file);
+    FilePtr nsrl_file = new File();
+    nsrl_file->hash_sha1 = "TEST_HASH";
+    nsrl_file->path = "TEST_PATH";
+    nsrl_file->IsInDB = true;
+    OurDatabase.FillTheDB(nsrl_file);
+
+    FilePtr not_nsrl_file = new File();
+    not_nsrl_file->hash_sha1 = "TEST_HASH";
+    not_nsrl_file->path = "TEST_PATH";
+    not_nsrl_file->IsInDB = false;
+    OurDatabase.FillTheDB(not_nsrl_file);
 }
