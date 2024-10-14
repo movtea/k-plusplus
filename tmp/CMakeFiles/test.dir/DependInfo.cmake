3af355f0cabd692121f76cecdd3e4bcc7f5811f4
PapuaGvinea145 <p0chta1234prosto@yandex.ru>
2024-10-14 09:56:57 +0500

    start of great cmake migration

---
+++ tmp/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/panzerfaust/Документы/proga/semen5/kursach/k-plusplus/src/OutputDB/outputDB.cpp" "CMakeFiles/test.dir/src/OutputDB/outputDB.cpp.o" "gcc" "CMakeFiles/test.dir/src/OutputDB/outputDB.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
